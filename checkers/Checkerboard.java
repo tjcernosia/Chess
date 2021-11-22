d3d316970161fb2b2ed890f97c2a24a1b67eb972
tjcernosia <77982051+tjcernosia@users.noreply.github.com>
2021-11-21 20:23:55 -0500

    fixes

--- checkers/Checkerboard.java
+++ checkers/Checkerboard.java
@@ -1,7 +1,20 @@
+import java.util.*;
+
 public class Checkerboard{
   
-  public Checkerboard(){
+	private Piece[][] board;
+	private boolean turn;
+	
+	public Checkerboard(){
     
-  }
+		init();
+		
+	}
+  
+	public void init(){
+		
+		board = new Piece[8][8];
+		
+	}
   
 }
---
+++ checkers/Piece.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Piece{
+	
+	private boolean isCrowned;
+	private int x;
+	private int y;
+	
+	public Piece(){
+		
+	}
+	
+	public Set generateMoves(Checkerboard b){
+		
+		return null;
+		
+	}
+	
+}
