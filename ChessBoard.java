e0c25b73a6633f003e8e43fa2620843ac6fffad5
tjcernosia <77982051+tjcernosia@users.noreply.github.com>
2022-01-16 10:59:11 -0500

    Wrote initBoard()
    Wrote initBoard() to initialize pieces on the board. However need to write generateMoves() for rest of pieces, and am encountering issues with knight.generateMoves()

--- ChessBoard.java
+++ ChessBoard.java
@@ -5,23 +5,58 @@ public class ChessBoard{
 	Piece[][] board;
 	
 	public ChessBoard(){
+		initBoard();		
+	}
+	
+	private void initBoard(){
+		//initialize board
 		board = new Piece[8][8];
+		
+		//pawns
+		for(int i = 0; i < 8; i++){
+			board[1][i] = new Pawn(i,1,true);
+			board[6][i] = new Pawn(i,7,false);
+		}
+		
+		//rooks
+		board[0][0] = new Rook(0,0,true);
+		board[0][7] = new Rook(0,7,true);
+		board[7][0] = new Rook(7,0,false);
+		board[7][7] = new Rook(7,7,false);
+		
+		//knights
+		board[0][1] = new Knight(0,1,true);
+		board[0][6] = new Knight(0,5,true);
+		board[7][1] = new Knight(7,1,false);
+		board[7][6] = new Knight(7,5,false);
+		
+		//bishops
+		board[0][2] = new Bishop(0,2,true);
+		board[0][5] = new Bishop(0,5,true);
+		board[7][2] = new Bishop(7,2,false);
+		board[7][5] = new Bishop(7,5,false);
+		
+		//kings
+		board[0][3] = new King(0,3,true);
+		board[7][3] = new King(7,3,false);
+		
+		//queens
+		board[0][4] = new Queen(0,4,true);
+		board[7][4] = new Queen(7,4,false);
+		
+		
 	}
 	
-	private ChessBoard initBoard(){
-		return null;
-	}
-	
-	//possible moves
+	//calls polymorphic generateMoves() for all pieces
 	public Set<Move> generateMoves(){
 		
-		HashSet<Move> r = new HashSet<Move>();
+		Set<Move> r = new HashSet<Move>();
 		
 		for(Piece[] a: board){
 			for(Piece p: a){
 				
-				if(p != null)
-					Collections.addAll(p.generateMoves());
+				if(p != null && p.generateMoves() != null)
+					r.addAll(p.generateMoves());
 			
 			}
 		}
